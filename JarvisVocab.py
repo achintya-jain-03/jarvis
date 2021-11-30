cf528d7ea192615c797875600c5e7fc10b7f68c2
achintya100 <95289472+achintya100@users.noreply.github.com>
2021-11-30 23:21:14 +0530

    Add files via upload

---
+++ JarvisVocab.py
@@ -0,0 +1,19 @@
+#JarvisVocab.py
+"""
+This module contains collections of words as lists which allow diversity in human input
+"""
+
+greetings = ['hi','hello','yo','hola','hey','hay']
+affirmative = ['yes','yeah','sure',"I'd like that"]
+negative = ['no','nah','not','never']
+searchTerms = ['open','.com','wikipedia','wolfram','wolframalpha','maps','search','google']
+powerOptions = ['shutdown', 'options','power', 'reboot', 'restart']
+farewell = ['bye', 'goodbye', 'quit']
+music = ['music','start']
+gratitude = ['thanks','tanks','thank you']
+mails = ['email','send','mail','outlook']
+sql = ['mysql','sql','database']
+joke = ['funny','joke','laugh',]
+settings = ['update','settings','credentials','username','password','email','emailid']
+game = ['game','guess']
+toggles = ['input', 'type', 'rate']
